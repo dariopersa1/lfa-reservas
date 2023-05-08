326fd5c314be8934aadece6090432be586f98c56
Darío Zorrilla Berenguer <dario.zorrilla.berenguer@hva.nl>
2023-05-08 19:10:58 +0200

    Asignación de mesas

--- lfa_reservas_app/lib/main.dart
+++ lfa_reservas_app/lib/main.dart
@@ -23,9 +23,65 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'LFA Reservas App',
       theme: ThemeData(
-        primaryColor: Color.fromARGB(255, 33, 33, 33),
+        primaryColor: const Color.fromARGB(255, 33, 33, 33),
       ),
-      home: const MyHomePage(title: 'LFA Reservas App'),
+      home: BottomNavBar() /*const MyHomePage(title: 'LFA Reservas App')*/,
+    );
+  }
+}
+
+class BottomNavBar extends StatefulWidget {
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const MyHomePage(title: 'LFA Reservas App'),
+    StatsPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Booking',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pie_chart),
+            label: 'Stats',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class StatsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stats Page'),
+      ),
+      body: const Center(
+        child: Text('Stats Content'),
+      ),
     );
   }
 }
@@ -40,6 +96,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class Reservation {
+  final String docId;
   final String name;
   final String date;
   final int guests;
@@ -48,20 +105,24 @@ class Reservation {
   final String hour;
   final String comment;
   final List preferences;
+  String mesa;
 
   Reservation(
-      {required this.name,
+      {required this.docId,
+      required this.name,
       required this.date,
       required this.guests,
       required this.email,
       required this.comment,
       required this.tlf,
       required this.hour,
-      required this.preferences});
+      required this.preferences,
+      required this.mesa});
 
   factory Reservation.fromFirestore(DocumentSnapshot snapshot) {
     final data = snapshot.data() as Map<String, dynamic>;
     return Reservation(
+        docId: snapshot.id,
         name: data['nombre'] + ' ' + data['apellidos'],
         date: data['fecha'],
         guests: data['px'],
@@ -69,9 +130,54 @@ class Reservation {
         comment: data['comentario'],
         tlf: data['tlf'],
         hour: data['hora'],
-        preferences: data['alergenos']);
+        preferences: data['alergenos'],
+        mesa: data['mesa'] ?? "-");
   }
 }
+
+final mesasAvailable = [
+  "-",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28",
+  "29",
+  "30",
+  "31",
+  "32",
+  "33",
+  "34",
+  "35",
+  "36",
+  "37",
+  "38",
+  "39",
+  "40"
+];
 
 class ReservaInfoSheet extends StatelessWidget {
   final Reservation reservation;
@@ -79,8 +185,7 @@ class ReservaInfoSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      //width: 200.0,
-      height: 600.0,
+      height: 700.0,
       child: ListView(
         scrollDirection: Axis.vertical,
         shrinkWrap: true,
@@ -133,6 +238,36 @@ class ReservaInfoSheet extends StatelessWidget {
               title: const Text('Hora'),
               subtitle: Text(reservation.hour.toString()),
               visualDensity: const VisualDensity(vertical: -2)),
+          ListTile(
+            title: const Text('Mesa: '),
+            subtitle: DropdownButtonFormField(
+              menuMaxHeight: 550.0,
+              items: mesasAvailable.map((String mesa) {
+                return DropdownMenuItem(
+                    value: mesa,
+                    child: Row(
+                      children: <Widget>[
+                        Text(mesa),
+                      ],
+                    ));
+              }).toList(),
+              onChanged: (newMesa) {
+                reservation.mesa = newMesa.toString();
+                FirebaseFirestore.instance
+                    .collection("reservas")
+                    .doc(reservation.docId)
+                    .update({"mesa": reservation.mesa}).then((_) {
+                  print('Document updated successfully!');
+                }).catchError((error) {
+                  print('Error updating document: $error');
+                });
+              },
+              value: reservation.mesa,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+              ),
+            ),
+          ),
           const Spacer(),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -195,7 +330,7 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: isLoading
-            ? Center(child: CircularProgressIndicator())
+            ? const Center(child: CircularProgressIndicator())
             : Column(mainAxisAlignment: MainAxisAlignment.center, children: [
                 ListTile(
                   leading: Icon(Icons.date_range),
@@ -227,7 +362,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       return Card(
                         child: ListTile(
                           title: Text(reservation.name),
-                          subtitle: Text('Mesa: 2'),
+                          subtitle: Text('Mesa: ${reservation.mesa}'),
                           trailing: Text('${reservation.guests} px'),
                           onTap: () {
                             // Show the bottom sheet
@@ -237,7 +372,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                 return ReservaInfoSheet(
                                     reservation: reservation);
                               },
-                            );
+                            ).then((value) => _fetchReservations());
                           },
                         ),
                       );
